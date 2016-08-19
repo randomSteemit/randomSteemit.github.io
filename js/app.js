fe7cfb221444e4001b829523762f51de0d2cb586
Tyler Fletcher <tfletcher@sastampabay.org>
2016-08-19 11:04:18 -0400

    Changed How Data Is Pulled, Able To Generate Fresh Links

--- js/app.js
+++ js/app.js
@@ -3,40 +3,71 @@ var app = angular.module('app', ['ngMaterial', 'ui.bootstrap', 'ui.navbar']);
 
 app.controller('mainCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
 
-    steem.getState("/created", function (err, results) {
-        var randomArray = [];
-        var randomNumber = Math.floor(Math.random() * 20) + 1;
-
-        for (var item in results.content) {
-            randomArray.push(item);
-        }
-        var randomArticle = randomArray[randomNumber];
-        var articleUrl = results.content[randomArticle].url;
-        $scope.randomArticle = "http://steemit.com" + articleUrl;
+    // Set a timeout to countdown a scoped var
+    // For the "Seconds Left" on index.html
+    setTimeout(countDown, 1000);
+    var timerSeconds = 8;
+    function countDown() {
+        timerSeconds--;
+        if (timerSeconds > 0) {setTimeout(countDown, 1000)}
+        $scope.timer = timerSeconds;
         $scope.$apply();
-    })
+    }
 
-    setTimeout(function () {
-        OpenInNewTab($scope.randomArticle);
-    }, 8000)
+    // Call the page load data handler and set the data
+    getData(handleDataTimeout);
 
+    // Data handler for page load
+    function handleDataTimeout(articleUrl) {
+        $scope.randomArticleUrl = "http://steemit.com" + articleUrl;
+        openPageDelayed();
+    }
+
+    // Receiving Data
+    function getData(callback) {
+        steem.getState("/created", function (err, results) {
+            var randomArray = [];
+            var randomNumber = Math.floor(Math.random() * 20) + 1;
+
+            for (var item in results.content) {randomArray.push(item)}
+
+            var randomArticleTitle = randomArray[randomNumber];
+            var articleUrl = results.content[randomArticleTitle].url;
+            callback(articleUrl);
+        })
+    }
+
+    // function to actullay open the new article
     function OpenInNewTab(url) {
         var win = window.open(url, '_blank');
         win.focus();
     }
 
-
-    var n = 8;
-    setTimeout(countDown, 1000);
-
-    function countDown() {
-        n--;
-        if (n > 0) {
-            setTimeout(countDown, 1000);
-        }
-        $scope.timer = n;
-        $scope.$apply();
+    // Function to open the article after 8 seconds
+    // This happens immediatly when you hit the website
+    function openPageDelayed(){
+        //Set the page countdown to open the article
+        setTimeout(function () {
+            OpenInNewTab($scope.randomArticleUrl);
+        }, 8000)
     }
 
+    // Function to open the page without a delay
+    // This happens when you click the "Here" button
+    function openPageNow(){
+        OpenInNewTab($scope.randomArticleUrl);
+    }
+
+    // your data handler for button click
+    function handleDataOnClick(articleUrl) {
+        $scope.randomArticleUrl = "http://steemit.com" + articleUrl;
+        openPageNow();
+    }
+
+    // Function to generate a new random article with no delay
+    // This happens when you click the "Here" button
+    $scope.generateNewArticle = function(){
+        getData(handleDataOnClick);
+    };
 
 }]);
