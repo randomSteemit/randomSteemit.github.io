ad794468f3008dbc305bccf79eae669a25ad4330
Tyler <fletchertyler914@yahoo.com>
2016-08-26 23:49:49 -0400

    New RandomSteemian Feature

--- js/app.js
+++ js/app.js
@@ -4,15 +4,19 @@ var app = angular.module('app', ['ngMaterial', 'ui.bootstrap', 'ui.navbar']);
 app.controller('mainCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
 
     // Call the page load data handler and set the data
-    getData(handleDataTimeout);
+    getRandomArticle(handleDataTimeout);
+    getRandomSteemian(steemianOnClick);
 
     // Set a timeout to countdown a scoped var
     // For the "Seconds Left" on index.html
     setTimeout(countDown, 1000);
     var timerSeconds = 8;
+
     function countDown() {
         timerSeconds--;
-        if (timerSeconds > 0) {setTimeout(countDown, 1000)}
+        if (timerSeconds > 0) {
+            setTimeout(countDown, 1000)
+        }
         $scope.timer = timerSeconds;
         $scope.$apply();
     }
@@ -24,13 +28,41 @@ app.controller('mainCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
     }
 
     // Receiving Data
-    function getData(callback) {
-        steem.getState("/created", function (err, results) {
+    function getRandomArticle(callback) {
+        var stateObject = [
+            {
+                id: 0,
+                state: "/created"
+            },
+            {
+                id: 1,
+                state: "/hot"
+            },
+            {
+                id: 2,
+                state: "/trending"
+            },
+            {
+                id: 3,
+                state: "/trending30"
+            },
+            {
+                id: 4,
+                state: "/active"
+            },
+        ];
+
+        var randomState = Math.floor(Math.random() * (4 - 0 + 1)) + 0;
+
+        steem.getState(stateObject[randomState].state, function (err, results) {
             var randomArray = [];
             var randomNumber = Math.floor(Math.random() * 20) + 1;
             console.log("Random Number: " + randomNumber);
+            console.log(results);
 
-            for (var item in results.content) {randomArray.push(item)}
+            for (var item in results.content) {
+                randomArray.push(item)
+            }
 
             var randomArticleTitle = randomArray[randomNumber];
             var articleUrl = results.content[randomArticleTitle].url;
@@ -39,37 +71,56 @@ app.controller('mainCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
         })
     }
 
-    // function to actullay open the new article
-    function OpenInNewTab(url) {
-        var win = window.open(url, '_blank');
-        win.focus();
+    function getRandomSteemian(callback) {
+        var totalAccount = 78000;
+
+        steem.lookupAccounts("", totalAccount, function (err, result) {
+            var randomNumber = Math.floor(Math.random() * (totalAccount - 0 + 1)) + 0;
+            var randomSteemian = result[randomNumber];
+            callback(randomSteemian);
+        })
     }
 
     // Function to open the article after 8 seconds
     // This happens immediatly when you hit the website
-    function openPageDelayed(){
+    function openPageDelayed() {
         //Set the page countdown to open the article
         setTimeout(function () {
-            OpenInNewTab($scope.randomArticleUrl);
+            var win = window.open($scope.randomArticleUrl, '_blank');
+            win.focus();
         }, 8000)
     }
 
-    // Function to open the page without a delay
-    // This happens when you click the "Here" button
-    function openPageNow(){
-        OpenInNewTab($scope.randomArticleUrl);
+    // Data handler for button click
+    function articleOnClick(articleUrl) {
+        $scope.randomArticleUrl = "http://steemit.com" + articleUrl;
     }
 
-    // your data handler for button click
-    function handleDataOnClick(articleUrl) {
-        $scope.randomArticleUrl = "http://steemit.com" + articleUrl;
-        //setTimeout(openPageNow(), 500); // fraction of a second delay
+    // Data handler for button click
+    function steemianOnClick(steemian) {
+        $scope.randomSteemianUrl = "https://steemit.com/@" + steemian;
     }
 
     // Function to generate a new random article with no delay
     // This happens when you click the "Here" button
-    $scope.generateNewArticle = function(){
-        getData(handleDataOnClick);
+    $scope.generateNewArticle = function () {
+        ga('send', {
+            hitType: 'event',
+            eventCategory: 'New Article',
+            eventAction: 'click',
+            eventLabel: 'New Article'
+        });
+        getRandomArticle(articleOnClick);
+    };
+
+    $scope.generateNewSteemian = function () {
+        ga('send', {
+            hitType: 'event',
+            eventCategory: 'New Article',
+            eventAction: 'click',
+            eventLabel: 'New Steemian'
+        });
+        getRandomSteemian(steemianOnClick);
     };
 
 }]);
